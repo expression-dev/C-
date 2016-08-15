2b9c72464c08a987131edcdf472e7d37332ff9b4
Alex Tsvetanov <ostrietoxxx@gmail.com>
2016-08-15 05:31:17 +0300

    1.4.0

--- main.cpp
+++ main.cpp
@@ -98,6 +98,40 @@ int main ()
 				std::cout << "\n\nError: " << asdf << std::endl;
 			}
 		}
+		else if (in.substr (0, 3) == "set")
+		{
+			int i;
+			for (i = 5 ; i < in.size () and in [i] != ' ' ; i ++) {}
+			string var_name = in.substr (4, i - 4);
+			to_solve = in.substr (i + 1);
+			try
+			{
+				std::string to_solve1 = transform (to_solve);
+				int vali = solver.calc (to_solve1);
+				std::stringstream val;
+				val << vali; 
+				{
+					string code = "template <typename T> T f ()\n{\n\treturn " + val.str () + ";\n}";
+					expr.addFunction (new function <int> (var_name, N));
+					solver = solve <int> (expr, parser);
+					std::ofstream out ("/tmp/new_op_fun_br.cpp");
+					out << code << "\n";
+					out.close ();
+					std::stringstream ss;
+					ss << "g++ -O2 -std=c++11 other/base3.cpp -o exec/" << N++ << ".exe";
+					system (ss.str ().c_str ());
+				}
+				std::stringstream ss;
+				ss << "echo \"<big>Set <big><big>\\`" + var_name + " = " << to_solve << " = " << vali << "\\` </big></big></big> <br><hr>\" >> interface/index.html\n";
+				std::cout << ss.str () << "\n";
+				system (ss.str ().c_str ());
+			}
+			catch (const char* asdf)
+			{
+				std::cout << "\n\nError: " << asdf << std::endl;
+			}
+		}
+
 		to_solve = "";
 		std::cout << "Next?\n";
 		getline (std::cin, in);
